578cd1fedcd2b3ebf66de2bf761e56372462741c
faizan <faizandedia@gmail.com>
2014-04-09 22:49:42 +0400

    Initial Commit

---
+++ SponsorPay/SponsorPayAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SponsorPayAppDelegate.h
+//  SponsorPay
+//
+//  Created by Faizan Ali on 4/9/14.
+//  Copyright (c) 2014 SponsorPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SponsorPayAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SponsorPay/SponsorPayViewController.m
@@ -0,0 +1,29 @@
+//
+//  SponsorPayViewController.m
+//  SponsorPay
+//
+//  Created by Faizan Ali on 4/9/14.
+//  Copyright (c) 2014 SponsorPay. All rights reserved.
+//
+
+#import "SponsorPayViewController.h"
+
+@interface SponsorPayViewController ()
+
+@end
+
+@implementation SponsorPayViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
